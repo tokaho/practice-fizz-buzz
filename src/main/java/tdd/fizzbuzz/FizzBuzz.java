38b06b43bb330360822139c32d64ae47fe136b5b
tokaho <tokaho2000@gmail.com>
2022-12-01 15:07:40 +0800

    [Refactor] introducee constant String for (EMPTY:"", FIZZ:"Fizz", BUZZ:"Buzz", WHIZZ:"Whizz")

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,21 +2,26 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
-    public String countOff(int i){
-        String returnStr="";
-        if(i%3==0){
-            returnStr+="Fizz";
+    private static final String EMPTY = "";
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String WHIZZ = "Whizz";
+
+    public String countOff(int i) {
+        String returnStr = EMPTY;
+        if (i % 3 == 0) {
+            returnStr += FIZZ;
         }
-        if(i%5==0){
-            returnStr+="Buzz";
+        if (i % 5 == 0) {
+            returnStr += BUZZ;
         }
-        if(i%7==0){
-            returnStr+="Whizz";
+        if (i % 7 == 0) {
+            returnStr += WHIZZ;
         }
 
-        if(returnStr.equals("")){
+        if (returnStr.equals(EMPTY)) {
             return String.valueOf(i);
-        }else{
+        } else {
             return returnStr;
         }
     }
